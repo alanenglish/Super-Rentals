ceb790e400cba7db94481d94f6e151a39828b709
Alan English <aenglish@manorhillcapital.com>
2018-12-15 12:11:23 -0600

    add hard-coded rental data, create each handlebar helper in rentals template to display each rental, install mirage and tutorial styles from ember cli, add test to check if rentals page lists all available rentals

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,37 +3,40 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('should show rentals as the home page', function(assert) {
+test('should show rentals as the home page', (assert) => {
   visit('/');
-  andThen(function() {
+  andThen(() => {
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 });
 
-test('should link to information about the company', function(assert) {
+test('should link to information about the company', (assert) => {
   visit('/');
   click('a:contains("About")');
-  andThen(function() {
+  andThen(() => {
     assert.equal(currentURL(), '/about', 'should navigate to about page');
   });
 });
 
-test('should link to contact information', function(assert) {
+test('should link to contact information', (assert) => {
   visit('/');
   click('a:contains("Contact")');
-  andThen(function() {
+  andThen(() => {
     assert.equal(currentURL(), '/contact', 'should navigate to contact page');
   })
 });
 
-test('should list available rentals', function(assert) {
+test('should list available rentals', (assert) => {
+  visit('/');
+  andThen(() => {
+    assert.equal(find('.listing').length, 3, 'should see three listings');
+  })
+});
+
+test('should filter the list of rentals by city', (assert) => {
 
 });
 
-test('should filter the list of rentals by city', function(assert) {
-
-});
-
-test('should show details for a selected rental', function(assert) {
+test('should show details for a selected rental', (assert) => {
 
 });
